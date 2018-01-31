696e7f01782efe7e645dd598b7cb92780e04d16d
atodoki@gmail.com <atodoki@gmail.com>
2018-01-31 15:11:44 -0800

    histograms for each category of data, and used some of the sample code from the book

--- HW1.R
+++ HW1.R
@@ -1,3 +1,23 @@
 ##### Data Science CS429 Homework 1 #####
 nyt1 <- read.csv("HW1_data/nyt1.csv")
 View(nyt1)
+
+install.packages("ggplot2")
+library(ggplot2)
+
+
+hist(nyt1$Age)
+hist(nyt1$Gender)
+hist(nyt1$Impressions)
+hist(nyt1$Clicks)
+hist(nyt1$Signed_In)
+
+# Example code from "Doing Data Science"
+nyt1$agecat <- cut(nyt1$Age, c(-Inf,18,24,34,54,64,Inf))
+hist(as.integer(nyt1$agecat))
+
+ggplot(nyt1, aes(x=Impressions, fill=agecat))+
+  geom_histogram(binwidth=1)
+
+ggplot(nyt1, aes(x=agecat, y=Impressions, fill=agecat))+
+  geom_boxplot()
