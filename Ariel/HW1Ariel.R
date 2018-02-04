c53906496259952763572fb9f8b6afc332573a0b
atodoki@gmail.com <atodoki@gmail.com>
2018-02-03 17:26:39 -0800

    make bar plots to compare data from multiple days

--- Ariel/HW1Ariel.R
+++ Ariel/HW1Ariel.R
@@ -1,13 +1,28 @@
 ##### Data Science CS429 Homework 1 #####
-nytData <- read.csv("HW1_data/nyt1.csv")
+
+day1 <- 1
+day2 <- 6
+day3 <- 8
+day4 <- 10
+day5 <- 27
+day6 <- 13
+day7 <- 20
+nytData <- read.csv(paste0("HW1_data/nyt",toString(day1),".csv"))
 View(nytData)
 str(nytData)
+
+nytData2 <- read.csv(paste0("HW1_data/nyt",toString(day2),".csv"))
+nytData3 <- read.csv(paste0("HW1_data/nyt",toString(day3),".csv"))
+nytData4 <- read.csv(paste0("HW1_data/nyt",toString(day4),".csv"))
+nytData5 <- read.csv(paste0("HW1_data/nyt",toString(day5),".csv"))
+nytData6 <- read.csv(paste0("HW1_data/nyt",toString(day6),".csv"))
+nytData7 <- read.csv(paste0("HW1_data/nyt",toString(day7),".csv"))
 
 install.packages("ggplot2")
 library(ggplot2)
 
 # Initial histograms of each variable
-hist(nytData$Age)
+hist(nytData$Age , col = "skyblue")
 hist(nytData$Gender, breaks=2)
 hist(nytData$Impressions)
 hist(nytData$Clicks)
@@ -18,13 +33,14 @@ nytData$age_group <- cut(nytData$Age, c(-Inf,18,24,34,54,64,Inf))
 hist(as.integer(nytData$age_group))
 
 # Using all data
-# factor gender 
-nytData$genderFactor[nytData$Gender == 0] <- "Male"
-nytData$genderFactor[nytData$Gender == 1] <- "Female"
+# factor gender
+nytData$genderFactor[nytData$Gender == 0 & nytData$Signed_In == 1] <- "Female"
+nytData$genderFactor[nytData$Gender == 0 & nytData$Signed_In == 0] <- "Unknown"
+nytData$genderFactor[nytData$Gender == 1] <- "Male"
 nytData$genderFactor <- factor(nytData$genderFactor)
 
-ggplot(nytData, aes(x=Impressions, fill = age_group))+geom_histogram(binwidth = 1)
-ggplot(nytData, aes(x=Impressions, fill = genderFactor))+geom_histogram(binwidth = 1) # is gender 0 for people who did not sign in?
+ggplot(nytData, aes(x=Impressions, fill = age_group))+geom_histogram(binwidth = 1)+labs(title = "Histogram of Impressions by Age Group", fill = "Age Group")
+ggplot(nytData, aes(x=Impressions, fill = genderFactor))+geom_histogram(binwidth = 1)+labs(title = "Histogram of Impressions by Gender", fill = "Gender")
 
 ####### Data of people who signed in #######
 signIn <- subset(nytData, Signed_In==1)
@@ -47,7 +63,7 @@ lines(xfit, yfit, col="blue", lwd=2)
 
 
 # CTR (click through rate)
-ggplot(subset(signIn,Impressions>0 & Clicks != 0), aes(x=Clicks/Impressions, fill=age_group))+geom_histogram(binwidth = 0.04) + labs(title="Click Through Rate (CTR)\n(does not include 0 clicks)")
+ggplot(subset(signIn,Impressions>0 & Clicks != 0), aes(x=Clicks/Impressions, fill=age_group))+geom_histogram(binwidth = 0.05) + labs(title="Click Through Rate (CTR)\n(does not include 0 clicks)")
 
 # gender categorization
 gen_click_counts <- table(signIn$Gender, signIn$Clicks)
@@ -70,6 +86,56 @@ ggplot(subset(nytData, Clicks>0), aes(x=Clicks, fill = impcat))+geom_histogram(b
 ggplot(subset(nytData, Clicks>1), aes(x=Clicks, fill = impcat))+geom_histogram(binwidth = 1)+labs(title="Histogram of Clicks",fill="# of Impressions")
 ggplot(subset(nytData, Clicks>2), aes(x=Clicks, fill = impcat))+geom_histogram(binwidth = 1)+labs(title="Histogram of Clicks",fill="# of Impressions")
 ggplot(subset(nytData, Clicks>3), aes(x=Clicks, fill = impcat))+geom_histogram(binwidth = 1)+labs(title="Histogram of Clicks",fill="# of Impressions")
+
+
+#### Plots on multiple days
+dayNames <- c(day1, day2, day3, day4, day5, day6, day7)
+
+clicks1 <- sum(nytData$Clicks)
+clicks2 <- sum(nytData2$Clicks)
+clicks3 <- sum(nytData3$Clicks)
+clicks4 <- sum(nytData4$Clicks)
+clicks5 <- sum(nytData5$Clicks)
+clicks6 <- sum(nytData6$Clicks)
+clicks7 <- sum(nytData7$Clicks)
+
+totalClicks <- c(clicks1, clicks2, clicks3, clicks4, clicks5, clicks6, clicks7)
+names(totalClicks) <- dayNames
+barplot(totalClicks, col=c("lightgreen", "lightcoral"), main = "Number of Clicks Per Day", xlab = "Day")
+
+imp1 <- sum(nytData$Impressions)
+imp2 <- sum(nytData2$Impressions)
+imp3 <- sum(nytData3$Impressions)
+imp4 <- sum(nytData4$Impressions)
+imp5 <- sum(nytData5$Impressions)
+imp6 <- sum(nytData6$Impressions)
+imp7 <- sum(nytData7$Impressions)
+
+totalImp <- c(imp1, imp2, imp3, imp4, imp5, imp6,imp7)
+names(totalImp) <- dayNames
+barplot(totalImp, col=c("lightgreen", "lightcoral"), main = "Number of Impressions Per Day", xlab = "Day")
+
+sign1 <- sum(nytData$Signed_In)
+sign2 <- sum(nytData2$Signed_In)
+sign3 <- sum(nytData3$Signed_In)
+sign4 <- sum(nytData4$Signed_In)
+sign5 <- sum(nytData5$Signed_In)
+sign6 <- sum(nytData6$Signed_In)
+sign7 <- sum(nytData7$Signed_In)
+
+totalSign <- c(sign1, sign2, sign3, sign4, sign5, sign6, sign7)
+names(totalSign) <- dayNames
+barplot(totalSign, col=c("lightgreen", "lightcoral"), main = "Number of Sign Ins Per Day", xlab = "Day")
+
+# Plot the total number of people per day
+totalVisits <- c(nrow(nytData),nrow(nytData2),nrow(nytData3),nrow(nytData4),nrow(nytData5),nrow(nytData6),nrow(nytData7))
+names(totalVisits) <- dayNames
+barplot(totalVisits, col=c("lightgreen", "lightcoral"), main = "Number of Visitors Per Day", xlab = "Day")
+
+# Plot CTR for each day
+totalCTR <- c(clicks1/imp1,clicks2/imp2,clicks3/imp3,clicks4/imp4,clicks5/imp5,clicks6/imp6,clicks7/imp7)
+names(totalCTR) <- dayNames
+barplot(totalCTR, col=c("lightgreen", "lightcoral"), main = "Click Through Rate Per Day", xlab = "Day")
 
 ######################### Example code from "Doing Data Science" #####################
 
