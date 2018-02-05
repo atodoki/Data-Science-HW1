81c693c89d304ab3d0eaa160419ab1143d65c2cd
atodoki@gmail.com <atodoki@gmail.com>
2018-02-05 01:04:56 -0800

    add for loop (written by Thomas) to read in all data, use apply functions and create barplots for the variables across the whole month

--- Ariel/HW1Ariel.R
+++ Ariel/HW1Ariel.R
@@ -1,36 +1,51 @@
 ##### Data Science CS429 Homework 1 #####
 
-day1 <- 1
-day2 <- 6
-day3 <- 8
-day4 <- 10
-day5 <- 27
-day6 <- 13
-day7 <- 20
-nytData <- read.csv(paste0("HW1_data/nyt",toString(day1),".csv"))
-View(nytData)
-str(nytData)
+## Install and load these packages if have not already done so
+# install.packages("ggplot2")
+# library(ggplot2)
 
-nytData2 <- read.csv(paste0("HW1_data/nyt",toString(day2),".csv"))
-nytData3 <- read.csv(paste0("HW1_data/nyt",toString(day3),".csv"))
-nytData4 <- read.csv(paste0("HW1_data/nyt",toString(day4),".csv"))
-nytData5 <- read.csv(paste0("HW1_data/nyt",toString(day5),".csv"))
-nytData6 <- read.csv(paste0("HW1_data/nyt",toString(day6),".csv"))
-nytData7 <- read.csv(paste0("HW1_data/nyt",toString(day7),".csv"))
+# Read in all 31 csv files into a list of data frames
+datalist <- list()
+for (i in 1:31){
+  datapoint <- read.csv(paste0("HW1_data/nyt",toString(i),".csv"))
+  datalist[[i]]<-datapoint 
+}
 
-install.packages("ggplot2")
-library(ggplot2)
+# day1 <- 25
+# day2 <- 26
+# day3 <- 27
+# day4 <- 28
+# day5 <- 29
+# day6 <- 30
+# day7 <- 31
+# nytData <- read.csv(paste0("HW1_data/nyt",toString(day1),".csv"))
+# View(nytData)
+# str(nytData)
+# 
+# nytData2 <- read.csv(paste0("HW1_data/nyt",toString(day2),".csv"))
+# nytData3 <- read.csv(paste0("HW1_data/nyt",toString(day3),".csv"))
+# nytData4 <- read.csv(paste0("HW1_data/nyt",toString(day4),".csv"))
+# nytData5 <- read.csv(paste0("HW1_data/nyt",toString(day5),".csv"))
+# nytData6 <- read.csv(paste0("HW1_data/nyt",toString(day6),".csv"))
+# nytData7 <- read.csv(paste0("HW1_data/nyt",toString(day7),".csv"))
 
-# Initial histograms of each variable
-hist(nytData$Age , col = "skyblue")
-hist(nytData$Gender, breaks=2)
-hist(nytData$Impressions)
-hist(nytData$Clicks)
-hist(nytData$Signed_In, breaks = 2)
 
 # categorize age_group
-nytData$age_group <- cut(nytData$Age, c(-Inf,18,24,34,54,64,Inf))
-hist(as.integer(nytData$age_group))
+for(i in 1:31){
+  datalist[[i]]$age_group <- cut(datalist[[i]]$Age, c(-Inf,18,24,34,54,64,Inf))
+}
+
+
+# Initial histograms of each variable
+initHistograms <- function(nytData){
+  hist(nytData$Age , col = "skyblue")
+  hist(nytData$Gender, breaks=2, col = "skyblue")
+  hist(nytData$Impressions, col = "skyblue")
+  hist(nytData$Clicks, col="skyblue")
+  hist(nytData$Signed_In, breaks = 2, col="skyblue")
+  hist(as.integer(nytData$age_group))
+}
+
 
 # Using all data
 # factor gender
@@ -89,53 +104,85 @@ ggplot(subset(nytData, Clicks>3), aes(x=Clicks, fill = impcat))+geom_histogram(b
 
 
 #### Plots on multiple days
-dayNames <- c(day1, day2, day3, day4, day5, day6, day7)
+dayNames <- c(1:31)
 
-clicks1 <- sum(nytData$Clicks)
-clicks2 <- sum(nytData2$Clicks)
-clicks3 <- sum(nytData3$Clicks)
-clicks4 <- sum(nytData4$Clicks)
-clicks5 <- sum(nytData5$Clicks)
-clicks6 <- sum(nytData6$Clicks)
-clicks7 <- sum(nytData7$Clicks)
+clickList <- lapply(datalist, "[",,"Clicks")
+sumClick <- sapply(clickList,sum)
 
-totalClicks <- c(clicks1, clicks2, clicks3, clicks4, clicks5, clicks6, clicks7)
-names(totalClicks) <- dayNames
-barplot(totalClicks, col=c("lightgreen", "lightcoral"), main = "Number of Clicks Per Day", xlab = "Day")
+names(sumClick) <- dayNames
+barplot(sumClick, col=c("lightgreen", "lightcoral"), main = "Number of Clicks per Day", xlab = "Day")
 
-imp1 <- sum(nytData$Impressions)
-imp2 <- sum(nytData2$Impressions)
-imp3 <- sum(nytData3$Impressions)
-imp4 <- sum(nytData4$Impressions)
-imp5 <- sum(nytData5$Impressions)
-imp6 <- sum(nytData6$Impressions)
-imp7 <- sum(nytData7$Impressions)
+# clicks1 <- sum(nytData$Clicks)
+# clicks2 <- sum(nytData2$Clicks)
+# clicks3 <- sum(nytData3$Clicks)
+# clicks4 <- sum(nytData4$Clicks)
+# clicks5 <- sum(nytData5$Clicks)
+# clicks6 <- sum(nytData6$Clicks)
+# clicks7 <- sum(nytData7$Clicks)
+# 
+# totalClicks <- c(clicks1, clicks2, clicks3, clicks4, clicks5, clicks6, clicks7)
+# names(totalClicks) <- dayNames
+# barplot(totalClicks, col=c("lightgreen", "lightcoral"), main = "Number of Clicks Per Day", xlab = "Day")
 
-totalImp <- c(imp1, imp2, imp3, imp4, imp5, imp6,imp7)
-names(totalImp) <- dayNames
-barplot(totalImp, col=c("lightgreen", "lightcoral"), main = "Number of Impressions Per Day", xlab = "Day")
 
-sign1 <- sum(nytData$Signed_In)
-sign2 <- sum(nytData2$Signed_In)
-sign3 <- sum(nytData3$Signed_In)
-sign4 <- sum(nytData4$Signed_In)
-sign5 <- sum(nytData5$Signed_In)
-sign6 <- sum(nytData6$Signed_In)
-sign7 <- sum(nytData7$Signed_In)
+impList <- lapply(datalist, "[",,"Impressions")
+sumImp <- sapply(impList, sum)
 
-totalSign <- c(sign1, sign2, sign3, sign4, sign5, sign6, sign7)
-names(totalSign) <- dayNames
-barplot(totalSign, col=c("lightgreen", "lightcoral"), main = "Number of Sign Ins Per Day", xlab = "Day")
+names(sumImp) <- dayNames
+barplot(sumImp, col=c("lightgreen", "lightcoral"), main = "Number of Impressions per Day", xlab = "Day")
+
+
+# imp1 <- sum(nytData$Impressions)
+# imp2 <- sum(nytData2$Impressions)
+# imp3 <- sum(nytData3$Impressions)
+# imp4 <- sum(nytData4$Impressions)
+# imp5 <- sum(nytData5$Impressions)
+# imp6 <- sum(nytData6$Impressions)
+# imp7 <- sum(nytData7$Impressions)
+# 
+# totalImp <- c(imp1, imp2, imp3, imp4, imp5, imp6,imp7)
+# names(totalImp) <- dayNames
+# barplot(totalImp, col=c("lightgreen", "lightcoral"), main = "Number of Impressions Per Day", xlab = "Day")
+
+
+signInList <- lapply(datalist, "[",,"Signed_In")
+sumSignIn <- sapply(signInList, sum)
+
+names(sumSignIn) <- dayNames
+barplot(sumSignIn, col=c("lightgreen", "lightcoral"), main = "Number of Sign Ins per Day", xlab = "Day")
+
+
+# sign1 <- sum(nytData$Signed_In)
+# sign2 <- sum(nytData2$Signed_In)
+# sign3 <- sum(nytData3$Signed_In)
+# sign4 <- sum(nytData4$Signed_In)
+# sign5 <- sum(nytData5$Signed_In)
+# sign6 <- sum(nytData6$Signed_In)
+# sign7 <- sum(nytData7$Signed_In)
+# 
+# totalSign <- c(sign1, sign2, sign3, sign4, sign5, sign6, sign7)
+# names(totalSign) <- dayNames
+# barplot(totalSign, col=c("lightgreen", "lightcoral"), main = "Number of Sign Ins Per Day", xlab = "Day")
 
 # Plot the total number of people per day
-totalVisits <- c(nrow(nytData),nrow(nytData2),nrow(nytData3),nrow(nytData4),nrow(nytData5),nrow(nytData6),nrow(nytData7))
+totalVisits <- sapply(datalist, nrow)
 names(totalVisits) <- dayNames
 barplot(totalVisits, col=c("lightgreen", "lightcoral"), main = "Number of Visitors Per Day", xlab = "Day")
 
+
+# totalVisits <- c(nrow(nytData),nrow(nytData2),nrow(nytData3),nrow(nytData4),nrow(nytData5),nrow(nytData6),nrow(nytData7))
+# names(totalVisits) <- dayNames
+# barplot(totalVisits, col=c("lightgreen", "lightcoral"), main = "Number of Visitors Per Day", xlab = "Day")
+
 # Plot CTR for each day
-totalCTR <- c(clicks1/imp1,clicks2/imp2,clicks3/imp3,clicks4/imp4,clicks5/imp5,clicks6/imp6,clicks7/imp7)
+totalCTR <- sumClick/sumImp
 names(totalCTR) <- dayNames
 barplot(totalCTR, col=c("lightgreen", "lightcoral"), main = "Click Through Rate Per Day", xlab = "Day")
+
+
+# totalCTR <- c(clicks1/imp1,clicks2/imp2,clicks3/imp3,clicks4/imp4,clicks5/imp5,clicks6/imp6,clicks7/imp7)
+# names(totalCTR) <- dayNames
+# barplot(totalCTR, col=c("lightgreen", "lightcoral"), main = "Click Through Rate Per Day", xlab = "Day")
 
 ######################### Example code from "Doing Data Science" #####################
 
